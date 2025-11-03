6b20610e90c6de6d36934aeeea5902bf15015b5b
Kristina Yancer <kristy.net.ua@mail.ru>
2025-11-04 01:01:09 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -1,3 +1,22 @@
+// Burger Menu
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const burgerMenu = document.querySelector(".header .header-button-menu");
+const burgerBody = document.querySelector(".header .header-button-menu__body");
+
+
+burgerMenu.addEventListener("click", (e) => {
+  header.classList.toggle("header-menu-open");
+  burgerBody.classList.toggle("active");
+  body.classList.toggle("lock");
+  e.stopPropagation();
+});
+
+
+$(".header-info__columns").clone().appendTo(".header-button-menu__body");
+$(".uni-columns .uni-columns__image").clone().appendTo(".uni-image--mobile");
+
+
 // Content Ranks Slider
 $('.content-ranks-slider').slick({
     slidesToShow: 3,
