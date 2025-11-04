95f21b3e5a4c87bca6a37d30bec6f7479772e52b
Kristina Yancer <kristy.net.ua@mail.ru>
2025-11-04 16:03:21 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -1,20 +1,46 @@
-// Burger Menu
-const body = document.querySelector("body");
-const header = document.querySelector(".header");
-const burgerMenu = document.querySelector(".header .header-button-menu");
-const burgerBody = document.querySelector(".header .header-button-menu__body");
+// // Burger Menu
+// const body = document.querySelector("body");
+// const header = document.querySelector(".header");
+// const burgerMenu = document.querySelector(".header .header-button-menu");
+// const burgerBody = document.querySelector(".header .header-button-menu__body");
 
 
-burgerMenu.addEventListener("click", (e) => {
-  header.classList.toggle("header-menu-open");
-  burgerBody.classList.toggle("active");
-  body.classList.toggle("lock");
-  e.stopPropagation();
+// burgerMenu.addEventListener("click", (e) => {
+//   header.classList.toggle("header-menu-open");
+//   burgerBody.classList.toggle("active");
+//   // body.classList.toggle("lock");
+//   e.stopPropagation();
+// });
+
+
+// $(".main-menu").clone().appendTo(".header-button-menu__body");
+// // $(".uni-columns .uni-columns__image").clone().appendTo(".uni-image--mobile");
+
+// Open menu
+const menuItems = document.querySelectorAll('.header-button-menu');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', (e) => {
+        e.stopPropagation();
+
+        const isActive = item.classList.contains('active');
+
+        menuItems.forEach(i => i.classList.remove('active'));
+
+        if (!isActive) {
+            item.classList.add('active');
+        }
+    });
+
+    document.addEventListener('click', (e) => {
+        if (!item.contains(e.target)) {
+            item.classList.remove('active');
+        }
+    });
+
+    $(".main-menu").clone().appendTo(".header-button-menu__body");
 });
 
-
-$(".header-info__columns").clone().appendTo(".header-button-menu__body");
-$(".uni-columns .uni-columns__image").clone().appendTo(".uni-image--mobile");
 
 
 // Content Ranks Slider
