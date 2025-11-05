c8131f8a2afb35068d307ab3ceaa780e91186002
Kristina Yancer <kristy.net.ua@mail.ru>
2025-11-05 11:20:45 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -1,21 +1,3 @@
-// // Burger Menu
-// const body = document.querySelector("body");
-// const header = document.querySelector(".header");
-// const burgerMenu = document.querySelector(".header .header-button-menu");
-// const burgerBody = document.querySelector(".header .header-button-menu__body");
-
-
-// burgerMenu.addEventListener("click", (e) => {
-//   header.classList.toggle("header-menu-open");
-//   burgerBody.classList.toggle("active");
-//   // body.classList.toggle("lock");
-//   e.stopPropagation();
-// });
-
-
-// $(".main-menu").clone().appendTo(".header-button-menu__body");
-// // $(".uni-columns .uni-columns__image").clone().appendTo(".uni-image--mobile");
-
 // Open menu
 const menuItems = document.querySelectorAll('.header-button-menu');
 
